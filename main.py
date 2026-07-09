838c06f71aa26bafb25ee1d6367a828e6cb04463
KSAN231 <Squidd3309@gmail.com>
2026-07-09 18:07:35 +0500

    Первая версия Дневника

---
+++ main.py
@@ -0,0 +1,110 @@
+import sys
+import json
+import os
+
+
+def load_entries():
+   if os.path.exists("test.json"):
+       with open("test.json", "r") as f:
+           data = json.load(f)
+           return data
+   else:
+       return []
+
+
+
+def menu():
+    print(" 1.Новая запись \n 2.Посмотреть записи \n 3.Найти запись \n 4.Редактирование записи \n 5.Удалить запись \n 6.Выход")
+
+def info_day(number, data, learning, timelearning, kpd):
+    print("\033[35m=================================\033[0m","\nID:", number,"\nДата:",data,"\nЧто изучал сегодня:", learning,"\nВремя:" , timelearning, "\nОценка дня:" , kpd,"\n\033[35m=================================\033[0m")
+count = 0
+entries = load_entries()
+while True:
+    menu()
+    answer = int(input("Выберете пункт:"))
+    if answer == 1: #1.Новая запись
+        count +=1
+        number = count
+        data = input("Дата")
+        learning = input("Что изучал сегодня?")
+        timelearning = input("Сколько времени занимался?(В минутах)")
+        kpd = input("Какая оценка дня?")
+        entry = {
+            "number": number,
+            "data": data,
+            "learning": learning,
+            "time": timelearning,
+            "kpd": kpd
+        }
+        entries.append(entry)
+        info_day(number, data, learning, timelearning, kpd)
+    elif answer == 2: # 2.Посмотреть записи
+        if len(entries) == 0:
+            print("Записей не найдено")
+        for i in entries:
+            info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+    elif answer == 3: #3.Найти запись
+        search = int(input("Введите номер ID:"))
+        found = False
+        for i in entries:
+            if i["number"] == search:
+                found = True
+                info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+                break
+        if found == False:
+            print("\033[31mЗапись не найдена😰\033[0m")
+    elif answer == 4: #4.Редактирование записи
+        id_search = int(input("Введите ID"))
+        found = False
+        for i in entries:
+            if i["number"] == id_search:
+                found = True
+                info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+                change = int(input("Что хотите изменить? \n1.Дата \n2.Что изучал \n3.Время изучения \n4.Оценка дня"))
+                if change == 1:
+                    change_data = input("Введите новое значение")
+                    i["data"] = change_data
+                    print("Данные обновлены")
+                    info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+                    break
+                elif change == 2:
+                    change_learning = input("Введите новое значение")
+                    i["learning"] = change_learning
+                    print("Данные обновлены")
+                    info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+                    break
+                elif change == 3:
+                    change_time = input("Введите новое значение")
+                    i["time"] = change_time
+                    print("Данные обновлены")
+                    info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+                    break
+                elif change == 4:
+                    change_kpd = input("Введите новое значение")
+                    i["kpd"] = change_kpd
+                    print("Данные обновлены")
+                    info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+                    break
+        if found == False:
+            print("\033[31mЗапись не найдена😰\033[0m")
+    elif answer == 5: #5.Удалить запись
+        id_search = int(input("Введите ID записи которую хотите удалить."))
+        found = False
+        for i in entries:
+            if i["number"] == id_search:
+                found = True
+                info_day(i["number"], i["data"], i["learning"], i["time"], i["kpd"])
+                confirmation = int(input("Подтвердите удаление 1(Да), 2(Нет)"))
+                if confirmation == 1:
+                    entries.remove(i)
+                    print("\033[32mЗапись удалена!\033[0m")
+                    break
+                elif confirmation == 2:
+                    print("\033[31mУдаление прервано!\033[0m")
+                    break
+        if found == False:
+            print("\033[31mЗапись не найдена😰\033[0m")
+    elif answer == 6: #6.Выход
+        sys.exit(1)
+
